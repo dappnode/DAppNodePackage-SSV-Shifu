e747f414ce5ce8cde6947875739caaf53070b9bb
mgarciate <mgarciate@gmail.com>
2022-11-02 20:57:28 +0100

    Add p2p ports into the config.yml

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -4,6 +4,8 @@ yq e -i '.db.Path = strenv(DB_PATH)' ./config.yml
 yq e -i '.eth2.Network = strenv(NETWORK)' ./config.yml
 yq e -i '.eth2.BeaconNodeAddr = strenv(BEACON_NODE_ADDR)' ./config.yml
 yq e -i '.eth1.ETH1Addr = strenv(ETH1ADDR)' ./config.yml
+yq e -i '.p2p.TCPPort = strenv(P2P_TCP_PORT)' ./config.yml
+yq e -i '.p2p.UDPPort = strenv(P2P_UDP_PORT)' ./config.yml
 yq e -i '.OperatorPrivateKey = strenv(OPERATOR_PRIVATE_KEY)' ./config.yml
 yq e -i '.global.LogLevel = strenv(LOG_LEVEL)' ./config.yaml
 
